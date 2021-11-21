4814ec16dda4bd28ac8b609ecb479d67cfa05731
DevilBringger <94440639+DevilBringger@users.noreply.github.com>
2021-11-21 21:17:23 +0530

    Add files via upload

---
+++ 3_Implementation/src/BankingSystem.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <conio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "BankingSystem.h"
+// #include "main.c"
+
+// Main Program starts execution from HERE
+void main(){
+    printf("\nThis demo program is brought you by %s",Author);
+    printf("\n\n\t\t\t\xB2\xB2\xB2\xB2\xB2\xB2  Banking System  \xB2\xB2\xB2\xB2\xB2\xB2");
+    // printf("\n\t\t\tEnter your name: ");
+    // gets(NAME);
+    // printf("\n\t\t\tEnter your account number: ");
+    // scanf("%d", &Acc);
+    login();
+    
+}
+
